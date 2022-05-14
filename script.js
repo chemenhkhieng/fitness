d53b2b32ac25b8d780e995f07e9ca90233dc9187
chemenhkhieng <chemenhkhieng2000@gmail.com>
2022-05-14 13:42:08 +0700

    The frist project

---
+++ script.js
@@ -0,0 +1,77 @@
+let menu = document.querySelector('#menu-btn')
+let navbar = document.querySelector('.header .navbar')
+
+menu.onclick = function(){
+    menu.classList.toggle('fa-times')
+    navbar.classList.toggle('active')
+};
+
+window.onscroll = function(){
+    menu.classList.remove('fa-times')
+    navbar.classList.remove('active')
+};
+
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 20,
+    effect: "fade",
+    grabCursor: true,
+    loop: true,
+    centeredSlider: true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+});
+
+var swiper = new Swiper(".review-slider", {
+    spaceBetween: 20,
+    grabCursor: true,
+    loop: true,
+    centeredSlider: true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    breakpoints:{
+        0:{
+            slidesPerView: 1,
+        },
+        600:{
+            slidesPerView: 2,
+        },
+    },
+});
+
+var swiper = new Swiper(".blogs-slider", {
+    spaceBetween: 20,
+    grabCursor: true,
+    loop: true,
+    centeredSlider: true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints:{
+        0:{
+            slidesPerView: 1,
+        },
+        768:{
+            slidesPerView: 2,
+        },
+        991:{
+            slidesPerView: 3,
+        },
+    }
+});
